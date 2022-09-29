051db957f69fdcc94fdb83deb93004225a9b85ae
justinraj89 <rajewskijustin@gmail.com>
2022-09-29 14:31:58 -0700

    remove from watchlist overlay stubbed up

--- src/components/WatchlistMovie/WatchlistMovie.js
+++ src/components/WatchlistMovie/WatchlistMovie.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./WatchlistMovie.css";
 import { LinkContainer } from "react-router-bootstrap";
+import Button from 'react-bootstrap/Button';
+import './WatchlistMovie.css'
 
 //============================================================
 
@@ -12,16 +14,22 @@ const WatchListMovie = (props) => {
   //==========================================================
 
   return (
+    <>
+    
     <div className="movie image-container">
       <LinkContainer to={linkUrl + movieId}>
         <img src={props.movieImg} alt={props.movieTitle}></img>
       </LinkContainer>
-
-      <div className="overlay d-flex align-items-center justify-content-center">
-        Details
+      <div className="removeOverlay d-flex align-items-center justify-content-center">
+        Remove from your Watchlist
       </div>
     </div>
+    </>
+    
   );
 };
 
 export default WatchListMovie;
+
+
+
