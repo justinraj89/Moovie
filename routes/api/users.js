5b330e3a0bd42e06dcc161875bc75cccd4077029
justinraj89 <rajewskijustin@gmail.com>
2023-01-23 23:26:33 -0800

    route import passport fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/users');
-const passport = require("passport");
 //=======================================
 
 
